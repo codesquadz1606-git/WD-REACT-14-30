d130b6b41385174448698e9811f13083f7737ae7
codesquadz1606-git <codesquadz1606@gmail.com>
2026-05-20 14:26:00 +0530

    Class 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import First, { First1 } from "./Class 1 -- Intro/First";
+
+export default function App(){
+  return(
+    <>
+      <h1>Hello</h1>
+      <First/>
+      <First1/>
+    </>
+  )
+}
---
+++ src/Class 1 -- Intro/First.jsx
@@ -0,0 +1,15 @@
+function First() {
+    return (
+        <h1>First Component.</h1>
+    )
+}
+
+export default First;
+
+function First1() {
+    return (
+        <h1>Second Component</h1>
+    )
+}
+
+export { First1 }; // named export
